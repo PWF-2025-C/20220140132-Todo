b675587c77a01b7981821a98a8e5443b867ae767
dewikhulbi <dewikhlb20@gmail.com>
2025-06-14 16:42:28 +0700

    api : logout, create, update, delete data

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Dedoc\Scramble\Scramble;
+use Dedoc\Scramble\Support\Generator\OpenApi;
+use Dedoc\Scramble\Support\Generator\SecurityScheme;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Routing\Route;
 use Illuminate\Support\Facades\Gate;
@@ -36,6 +38,10 @@ class AppServiceProvider extends ServiceProvider
 
         Scramble::configure()->routes(function (Route $route) {
             return Str::startsWith($route->uri, 'api/');
-});
+        })->withDocumentTransformers(function (OpenApi $openApi){
+            $openApi->secure(
+                SecurityScheme::http('bearer')
+            );
+        });
 }
 }
---
+++ tugas12.md
@@ -0,0 +1,20 @@
+login
+![alt text](<Screenshot/tugas 12/Screenshot 2025-06-14 154958.png>)
+
+logout
+![alt text](image-5.png)
+
+Endpoint untuk mengambil semua data Todo
+![alt text](image-6.png)
+
+Endpoint untuk menambah data Todo baru
+![alt text](image-7.png)
+
+Endpoint untuk mengubah Todo
+![alt text](image-8.png)
+
+Endpoint untuk menghapus Todo
+![alt text](image-9.png)
+
+Endpoint untuk melakukan pencarian pada Todo
+![alt text](image-10.png)
